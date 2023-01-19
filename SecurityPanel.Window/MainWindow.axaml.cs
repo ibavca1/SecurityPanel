6e8708c8e17efc262f6fd37d1b2d3a7909273fc3
Anton <ibavca@gmail.com>
2023-01-19 15:31:51 +0500

    view models

--- SecurityPanel.Window/MainWindow.axaml.cs
+++ SecurityPanel.Window/MainWindow.axaml.cs
@@ -1,3 +1,9 @@
+using System;
+using Avalonia;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using SecurityPanel.Window.ViewModels;
+
 namespace SecurityPanel.Window;
 
 public partial class MainWindow : Avalonia.Controls.Window
@@ -5,5 +11,6 @@ public partial class MainWindow : Avalonia.Controls.Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainViewModel();
     }
 }
